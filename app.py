d3bbc3be63d9d012bbe0a7597ff4396ce92ff15f
asus gaming <ne.villacisd@uea.edu.ec>
2025-02-15 14:21:03 -0500

    Tarea semana 9

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+    return f'Bienvenido, {nombre}!'
+
+if __name__ == '__main__':
+    app.run()
